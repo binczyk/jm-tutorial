dd24d1c5a0a54080aabf2ea2e1be4d0bfd30ae7c
unknown <daniel.biniek@stazysta.comarch.pl>
2015-07-06 09:09:56 +0200

    tsety kalkulatora

--- src/test/java/com/acme/calc/CalculatorTest.java
+++ src/test/java/com/acme/calc/CalculatorTest.java
@@ -1,5 +1,6 @@
 package com.acme.calc;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CalculatorTest {
@@ -8,11 +9,61 @@ public class CalculatorTest {
 
 	@Test
 	public void additionShouldReturnCorrectResult() {
-
+		calculator = new Calculator();
+		// given
+		double a = 5.0;
+		double b = 6.0;
+		// when
+		Double result = calculator.add(a, b);
+		// then
+		Assert.assertTrue(result == 11);
 	}
 
 	@Test
 	public void subtractionShouldReturnCorrectResult() {
+		calculator = new Calculator();
+		// given
+		double a = 5.0;
+		double b = 6.0;
+		// when
+		Double result = calculator.subtract(a, b);
+		// then
+		Assert.assertTrue(result == -1);
+	}
+	
+	@Test
+	public void multiplcstionShouldReturnCorrectResult(){
+		calculator = new Calculator();
+		// given
+		double a = 5.0;
+		double b = 6.0;
+		// when
+		Double result = calculator.multiply(a, b);
+		// then
+		Assert.assertTrue(result == 30);
+	}
+	
+	@Test
+	public void didvidetionShouldReturnCorrectResult(){
+		calculator = new Calculator();
+		// given
+		double a = 6.0;
+		double b = 6.0;
+		// when
+		Double result = calculator.divide(a, b);
+		// then
+		Assert.assertTrue(result == 1);
+	}
+	
+	@Test(expected = DivisorCannotBeZeroException.class)
+	public void DivisorShouldBeNot0(){
+		calculator = new Calculator();
+		// given
+		double a = 6.0;
+		double b = 0.0;
+		// when
+		Double result = calculator.divide(a, b);
+
 	}
 
 	// TODO division and multiplication test!
--- src/test/java/com/acme/order/PizzaOrderTest.java
+++ src/test/java/com/acme/order/PizzaOrderTest.java
@@ -3,4 +3,13 @@ package com.acme.order;
 public class PizzaOrderTest {
 
 	
+	private Customer customer;
+	private PizzaType pizzaType;
+	private PizzaOrder pizzaOrder = new PizzaOrder(customer, pizzaType);
+	
+	@Test
+	public void withEstimatedTimeTest(){
+		
+	}
+	
 }
